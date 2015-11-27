ef9c24edfd32ec08b5c14e42c8b97650bab96d0f
Matijs van Zuijlen <matijs@matijs.net>
2015-11-27 08:41:29 +0100

    Documnet GHtml2Pdf module

--- lib/ghtml2pdf.rb
+++ lib/ghtml2pdf.rb
@@ -1,7 +1,11 @@
 require 'ghtml2pdf/version'
 require 'ghtml2pdf/application'
 
+# Main entry point for GHtml2Pdf.
 module GHtml2Pdf
+  # Run the application.
+  #
+  # @param [Array] argv The command line arguments
   def self.run(argv)
     GHtml2Pdf::Application.new(argv).run
   end
