ac25903b4121b866c54ceb1f2dd056863ebfb438
Matijs van Zuijlen <matijs@matijs.net>
2015-11-27 15:24:08 +0100

    Move complexity into GHtml2Pdf module

--- lib/ghtml2pdf.rb
+++ lib/ghtml2pdf.rb
@@ -7,6 +7,10 @@ module GHtml2Pdf
   #
   # @param [Array] argv The command line arguments
   def self.run(argv)
+    Gtk.init
     GHtml2Pdf::Application.new(argv).run
+  rescue GHtml2Pdf::MissingArgument => e
+    warn e.message
+    exit 1
   end
 end
