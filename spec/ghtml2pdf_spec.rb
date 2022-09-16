427592935a813bd2d5003c8656b9f5990a91556c
Matijs van Zuijlen <matijs@matijs.net>
2022-09-16 10:48:32 +0200

    Fix RSpec/NoExpectationExample offenses

--- spec/ghtml2pdf_spec.rb
+++ spec/ghtml2pdf_spec.rb
@@ -6,8 +6,4 @@ describe GHtml2Pdf do
   it "has a version number" do
     expect(GHtml2Pdf::VERSION).not_to be_nil
   end
-
-  it "does something useful" do
-    skip "Think of something to test"
-  end
 end
